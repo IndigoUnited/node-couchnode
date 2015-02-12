13f14a81ccc1f278f3faceced17aa11a43b107cd
Marco Oliveira <me@marcooliveira.com>
2015-02-12 20:56:18 +0000

    [wip] savepoint

--- map.js
+++
@@ -1,8 +0,0 @@
-var async = require('async');
-var fs    = require('fs');
-
-async.map(['file1','file2','file3'], fs.stat, function(err, results){
-
-console.log(arguments);
-    // results is now an array of stats for each file
-});
